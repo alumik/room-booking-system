daaa059058b974be6117ba96e67c464f59e82ae3
Zhong Zhenyu <nczzy1997@gmail.com>
2018-06-14 14:52:20 +0800

    测试网络钩子

--- backend/views/site/about.php
+++ backend/views/site/about.php
@@ -16,6 +16,6 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= nl2br(Html::encode('本网站作者：钟震宇（网站） 张志毅（数据库/欢迎界面）。')) ?>
     </div>
 
-    <p>网站版本1.1</p>
+    <p>网站版本1.1.01</p>
 
 </div>
