16fdedbd4b1d15eee61fdd702aa316345b329962
Zhong Zhenyu <nczzy1997@gmail.com>
2018-07-04 15:26:36 +0800

    删除不必要的注释/调整部分参数位置

--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,5 @@
 <?php
 return [
-    'adminEmail' => 'admin@netsurfing.science',
-    'supportEmail' => 'support@netsurfing.science',
     'user.passwordResetTokenExpire' => 3600,
-    'version' => '1.4.4',
+    'version' => '1.4.5',
 ];
