2ec2c494ca28648a6a113d70d0606ecc2a0dee9b
Zhong Zhenyu <nczzy1997@gmail.com>
2020-07-27 16:48:10 +0800

    重构布局视图

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,100 +1,86 @@
 <?php
 
-/** @noinspection PhpUnhandledExceptionInspection */
-
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
-use yii\web\View;
 use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;
 use frontend\assets\AppAsset;
 
-/* @var $this View */
+/* @var $this yii\web\View */
 /* @var $content string */
-
-/* @author 钟震宇 <nczzy1997@gmail.com> */
 
 AppAsset::register($this);
 ?>
 
-<?php $this->beginPage() ?>
-
+<?php $this->beginPage(); ?>
 <!DOCTYPE html>
-
-<html lang="<?= Yii::$app->language ?>">
-
+<html lang="<?= Yii::$app->language; ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
+    <meta charset="<?= Yii::$app->charset; ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+    <?= Html::csrfMetaTags(); ?>
+    <title><?= Html::encode($this->title); ?></title>
+    <?php $this->head(); ?>
 </head>
-
 <body>
-
-<?php $this->beginBody() ?>
-
+<?php $this->beginBody(); ?>
 <div class="wrap">
     <?php
-        NavBar::begin([
-            'brandLabel' => Yii::$app->name,
-            'brandUrl' => Yii::$app->homeUrl,
-            'options' => [
-                'class' => 'navbar-inverse navbar-fixed-top',
-            ],
-        ]);
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
 
-        $menuItems = [];
+    $menuItems = [];
 
-        if (Yii::$app->user->isGuest) {
-            $menuItems[] = ['label' => '注册', 'url' => ['/site/signup']];
-            $menuItems[] = ['label' => '登录', 'url' => ['/site/login']];
-        } else {
-            $menuItems[] = ['label' => '预约房间', 'url' => ['/room/index']];
-            $menuItems[] = ['label' => '我的预约', 'url' => ['/application/index']];
-            $menuItems[] = ['label' => '我的账号', 'url' => ['/user/view']];
-            $menuItems[] = '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    '注销 (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>';
-        }
+    if (Yii::$app->user->isGuest) {
+        $menuItems[] = ['label' => '注册', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => '登录', 'url' => ['/site/login']];
+    } else {
+        $menuItems[] = ['label' => '预约房间', 'url' => ['/room/index']];
+        $menuItems[] = ['label' => '我的预约', 'url' => ['/application/index']];
+        $menuItems[] = ['label' => '我的账号', 'url' => ['/user/view']];
+        $menuItems[] = '<li>'
+            . Html::beginForm(['/site/logout'], 'post')
+            . Html::submitButton(
+                '注销 (' . Yii::$app->user->identity->username . ')',
+                ['class' => 'btn btn-link logout']
+            )
+            . Html::endForm()
+            . '</li>';
+    }
 
-        echo Nav::widget([
-            'options' => ['class' => 'navbar-nav navbar-right'],
-            'items' => $menuItems,
-        ]);
-        NavBar::end();
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => $menuItems,
+    ]);
+    NavBar::end();
     ?>
-
-    <?php if ($this->context->id=='site' && $this->context->action->id=='index') { ?>
+    <?php if ($this->context->id == 'site' && $this->context->action->id == 'index'): ?>
         <?= $content ?>
-    <?php } else { ?>
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-    <?php } ?>
+    <?php else: ?>
+        <div class="container">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
+    <?php endif; ?>
 </div>
-
-<?php if (!($this->context->id=='site' && $this->context->action->id=='index')) { ?>
-<footer class="footer">
-    <div class="container">
-        <p class="text-center">&copy; <?= Html::a(Yii::$app->name, ['/site/about']) ?> <?= date('Y') ?></p>
-    </div>
-</footer>
-<?php } ?>
-
-<?php $this->endBody() ?>
+<?php if (!($this->context->id == 'site' && $this->context->action->id == 'index')): ?>
+    <footer class="footer">
+        <div class="container">
+            <p class="text-center">&copy; <?= Html::a(Yii::$app->name, ['/site/about']) ?> <?= date('Y') ?></p>
+        </div>
+    </footer>
+<?php endif; ?>
+<?php $this->endBody(); ?>
 </body>
 </html>
-<?php $this->endPage() ?>
+<?php $this->endPage(); ?>
