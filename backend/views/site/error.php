74df363f75f65d21d2ba1511a203fd0e5fc8bc0e
Zhong Zhenyu <nczzy1997@gmail.com>
2020-07-27 16:11:53 +0800

    重构站点视图

--- backend/views/site/error.php
+++ backend/views/site/error.php
@@ -7,21 +7,14 @@ use yii\helpers\Html;
 /* @var $message string */
 /* @var $exception Exception */
 
-/* @author 钟震宇 <nczzy1997@gmail.com> */
-
 $this->title = $name;
 ?>
 
 <div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
+    <h1><?= Html::encode($this->title); ?></h1>
     <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+        <?= nl2br(Html::encode($message)); ?>
     </div>
-
     <p>上述错误信息表明该请求不能得到正确执行。</p>
-
     <p>如果你觉得这是服务器错误，请联系我们，谢谢。</p>
-
 </div>
--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -1,38 +1,32 @@
 <?php
 
-use common\models\UserLoginForm;
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-/* @var $model UserLoginForm */
-
-/* @author 钟震宇 <nczzy1997@gmail.com> */
+/* @var $model common\models\UserLoginForm */
 
 $this->title = '登录';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div class="site-login">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
+    <h1><?= Html::encode($this->title); ?></h1>
     <p>请输入工号和密码登录系统：</p>
-
     <div class="row">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-                <?= $form->field($model, 'admin_id')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'admin_id')->textInput(['autofocus' => true]); ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+            <?= $form->field($model, 'password')->passwordInput(); ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+            <?= $form->field($model, 'rememberMe')->checkbox(); ?>
 
-                <div class="form-group">
-                    <?= Html::submitButton('登录', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
+            <div class="form-group">
+                <?= Html::submitButton('登录', ['class' => 'btn btn-primary', 'name' => 'login-button']); ?>
+            </div>
 
             <?php ActiveForm::end(); ?>
         </div>
